e1659334d9ea117e3195a6290fca85063a33765a
joey <leej42>
2016-01-04 22:01:02 -0500

    removed things and populated seed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,30 +4,6 @@
 
 class MainController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.awesomeThings = [];
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
 }
 
 angular.module('venueApp')
