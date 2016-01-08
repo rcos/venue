a840c11d3a3e19bb24bf9d6b7344befc1a579b05
joey <leej42>
2016-01-08 10:37:32 -0500

    removed console logs

--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -5,15 +5,9 @@ angular.module('venueApp')
 
     Auth.getCurrentUser((user) => {
       $scope.user = user;
-      console.log($scope.user);
-
     });
 
-    console.log($scope.page)
-
     $scope.isActive = (pageview) => {
-      console.log(pageview);
-      console.log( $scope.page);
       return pageview == $scope.page;
     }
 
