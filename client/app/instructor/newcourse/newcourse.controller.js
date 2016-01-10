e703f622c6490731019ec0448c50bad08cc43bab
Severin Ibarluzea <seveibar@gmail.com>
2016-01-09 21:34:24 -0500

    new course page

--- client/app/instructor/newcourse/newcourse.controller.js
+++ client/app/instructor/newcourse/newcourse.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('NewcourseCtrl', ($scope) => {
-    
+  .controller('NewCourseCtrl', ($scope) => {
+    $scope.register = (form)=>{
+        $scope.submitted = true;
+        if (form.$valid) {
+          Course.create({
+            
+          }).then(() => {
+            $scope.success = true;
+          }).catch(() => {
+            err = err.data;
+            $scope.errors = {};
+
+            // Update validity of form fields that match the mongoose errors
+            angular.forEach(err.errors, (error, field) => {
+              form[field].$setValidity('mongoose', false);
+              $scope.errors[field] = error.message;
+            });
+          });
+        }
+    };
   });
--- client/app/instructor/newcourse/newcourse.js
+++ client/app/instructor/newcourse/newcourse.js
@@ -4,7 +4,7 @@ angular.module('venueApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/instructor/newcourse', {
-        templateUrl: 'app/instructor/newcourse/newcourse/newcourse.html',
-        controller: 'NewcourseCtrl'
+        templateUrl: 'app/instructor/newcourse/newcourse.html',
+        controller: 'NewCourseCtrl'
       });
   });
