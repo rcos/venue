f19a6b3aaebbe24d5a2d50fabde97c81ffcb1ab6
Severin Ibarluzea <seveibar@gmail.com>
2016-01-13 03:28:13 -0500

    student course page

--- client/app/student/courses/studentcourses.controller.js
+++ client/app/student/courses/studentcourses.controller.js
@@ -3,7 +3,6 @@
 angular.module('venueApp')
   .controller('StudentCoursesCtrl', function ($scope, User) {
     User.get({withCourses: true}, (user)=>{
-      console.log(user.courses);
       $scope.user = user;
     });
   });
