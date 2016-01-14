0f48133f88b6b98023bcc33bd00a481e9da62801
Joseph Lee <leej42@rpi.edu>
2016-01-14 02:04:55 -0500

    Update instructorcourses.controller.spec.js

--- client/app/instructor/courses/instructorcourses.controller.spec.js
+++ client/app/instructor/courses/instructorcourses.controller.spec.js
@@ -5,7 +5,7 @@ describe('Controller: InstructorCoursesCtrl', function () {
   // load the controller's module
   beforeEach(module('venueApp'));
 
-  var StudentCoursesCtrl, scope;
+  var InstructorCoursesCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
