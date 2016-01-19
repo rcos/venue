69153445969958093d021424cf846e7af8059f7c
Kiana McNellis <kiana@kmcnellis.com>
2016-01-19 18:43:18 -0500

    Initial google maps picker

--- client/components/eventform/eventform.controller.js
+++ client/components/eventform/eventform.controller.js
@@ -12,6 +12,155 @@ angular.module('venueApp')
     $scope.event.endDate = new Date();
     $scope.event.startDateOpen = false;
     $scope.event.endDateOpen = false;
+
+    $scope.place = {};
+    $scope.showPlaceDetails = function(param) {
+      $scope.place = param;
+    }
+
+    $scope.toggleMap = function () {
+      console.log("toggle");
+      $scope.searchbox.options.visible = !$scope.searchbox.options.visible
+    };
+
+    uiGmapGoogleMapApi.then(function(maps) {
+      console.log("google maps ready")
+      maps.visualRefresh = true;
+      $scope.defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(40.82148, -73.66450),
+        new google.maps.LatLng(40.66541, -74.31715));
+
+
+        $scope.map.bounds = {
+          northeast: {
+            latitude:$scope.defaultBounds.getNorthEast().lat(),
+            longitude:$scope.defaultBounds.getNorthEast().lng()
+          },
+          southwest: {
+            latitude:$scope.defaultBounds.getSouthWest().lat(),
+            longitude:-$scope.defaultBounds.getSouthWest().lng()
+
+          }
+        }
+        $scope.searchbox.options.bounds = new google.maps.LatLngBounds($scope.defaultBounds.getNorthEast(),     $scope.defaultBounds.getSouthWest());
+
+    });
+
+    $scope.selected= {
+      options: {
+        visible:false
+
+      },
+      templateurl:'window.tpl.html',
+      templateparameter: {}
+    };
+
+    $scope.map= {
+      control: {},
+      center: {
+        latitude: 40.74349,
+        longitude: -73.990822
+      },
+      zoom: 12,
+      dragging: false,
+      bounds: {},
+      markers: [],
+      idkey: 'place_id',
+      events: {
+        idle: function (map) {
+
+        },
+        dragend: function(map) {
+          //update the search box bounds after dragging the map
+          var bounds = map.getBounds();
+          var ne = bounds.getNorthEast();
+          var sw = bounds.getSouthWest();
+          $scope.searchbox.options.bounds = new google.maps.LatLngBounds(sw, ne);
+          //$scope.searchbox.options.visible = true;
+        }
+      }
+    };
+
+    $scope.searchbox= {
+      template: 'searchbox.tpl.html',
+      //position:'top-right',
+      position:'top-left',
+      options: {
+        bounds: {},
+        visible: true
+      },
+      //parentdiv:'searchBoxParent',
+      events: {
+        places_changed: function (searchBox) {
+
+          var places = searchBox.getPlaces()
+
+          if (places.length == 0) {
+            return;
+          }
+          // For each place, get the icon, place name, and location.
+          var newMarkers = [];
+          var bounds = new google.maps.LatLngBounds();
+          for (var i = 0, place; place = places[i]; i++) {
+            // Create a marker for each place.
+            var marker = {
+              id:i,
+              place_id: place.place_id,
+              name: place.name,
+              latitude: place.geometry.location.lat(),
+              longitude: place.geometry.location.lng(),
+              options: {
+                visible:false
+              },
+              templateurl:'window.tpl.html',
+              templateparameter: place,
+              closeClick: function() {
+                $scope.selected.options.visible = false;
+                marker.options.visble = false;
+                return $scope.$apply();
+              },
+              onClicked: function() {
+                $scope.selected.options.visible = false;
+                $scope.selected = marker;
+                $scope.selected.options.visible = true;
+              }
+            };
+            newMarkers.push(marker);
+
+            bounds.extend(place.geometry.location);
+          }
+
+          $scope.map.bounds = {
+            northeast: {
+              latitude: bounds.getNorthEast().lat(),
+              longitude: bounds.getNorthEast().lng()
+            },
+            southwest: {
+              latitude: bounds.getSouthWest().lat(),
+              longitude: bounds.getSouthWest().lng()
+            }
+          }
+
+          $scope.map.markers = newMarkers;
+        }
+      }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     EventInfo.getAll({}, (eventinfos) => {
       $scope.eventinfos = eventinfos;
     });
