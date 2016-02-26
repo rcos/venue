ea879e611ea7b34743b471986bb2a7113934b00b
Severin Ibarluzea <seveibar@gmail.com>
2016-02-26 13:52:36 -0500

    added links on student course page

--- client/app/student/courses/studentcourses.controller.js
+++ client/app/student/courses/studentcourses.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('StudentCoursesCtrl', function ($scope, User) {
+  .controller('StudentCoursesCtrl', function ($scope, User, $location) {
     User.get({withCourses: true}, (user)=>{
       $scope.user = user;
     });
+    $scope.goToCourse = function(course){
+      $location.path("/courses/" + course._id);
+    };
   });
