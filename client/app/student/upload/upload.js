f7784ede0b030907510e076b44e7281c97d939c7
Severin Ibarluzea <seveibar@gmail.com>
2016-02-29 16:43:20 -0500

    new upload process with event selection

--- client/app/student/upload/upload.js
+++ client/app/student/upload/upload.js
@@ -4,6 +4,10 @@ angular.module('venueApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/student/upload', {
+        templateUrl: 'app/student/upload/eventlist.html',
+        controller: 'UploadCtrl'
+      })
+      .when('/student/upload/:eventid', {
         templateUrl: 'app/student/upload/upload.html',
         controller: 'UploadCtrl'
       });
