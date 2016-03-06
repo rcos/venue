95f815919cbcfe61ba472f0cba7987e8d6c4688a
joey <leej42@rpi.edu>
2016-03-06 17:30:50 -0500

    instructor course page link

--- client/app/instructor/courses/instructorcourses.controller.js
+++ client/app/instructor/courses/instructorcourses.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('InstructorCoursesCtrl', function ($scope, User) {
+  .controller('InstructorCoursesCtrl', function ($scope, $location, User) {
     User.get({withCourses: true}, (user)=>{
       $scope.user = user;
     });
+
+    $scope.goToCourse = (course) => {
+      $location.path("/courses/" + course._id);
+    };
+ 
   });
