145ef63e2b7e966aea3b9d77d8fde179289d7cc0
Kiana McNellis <kiana@kmcnellis.com>
2016-03-16 22:38:59 -0400

    Change student section event page to use sectionevent endpoint to get nessesary data

--- client/app/student/events/studentevents.controller.js
+++ client/app/student/events/studentevents.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('StudentEventsCtrl', function ($scope, User) {
-    User.get({withEvents: true}, (user)=>{
-      $scope.user = user;
+  .controller('StudentEventsCtrl', function ($scope, User, SectionEvent) {
+    SectionEvent.getAll({onlyUserSections:'me',withEventInfo:true,withSection:true},(events)=>{
+      $scope.events = events;
     });
   });
