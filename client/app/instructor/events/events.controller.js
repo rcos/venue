079d59b3ef668506b7986e53c0b5288240147a7a
Kiana McNellis <kiana@kmcnellis.com>
2016-04-27 15:37:17 -0400

    Change instructor events to use card

--- client/app/instructor/events/events.controller.js
+++ client/app/instructor/events/events.controller.js
@@ -6,19 +6,12 @@ angular.module('venueApp')
     $scope.user = {};
     $scope.events = [];
 
-    User.get({withSections:true, withEvents: true, withSectionsCourse:true}, (user) => {
+    User.get({withEvents: true, withEventSections: true}, (user) => {
       $scope.user = user;
       $scope.sections = user.sections;
       $scope.events = user.events;
     });
 
-    $scope.deleteEvent = (event)=> {
-      if (confirm("This will also delete all sections submissions")) {
-        SectionEvent.delete({id:event._id}, (response)=>{
-          $scope.events.splice($scope.events.indexOf(event), 1);
-        })
-      }
-    }
     $scope.goToEvent = (event) => {
       $location.path("/events/" + event._id);
     };
