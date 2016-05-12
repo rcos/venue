2563d73e2c05a1a7c1b269a16286db379e779ae9
Kiana McNellis <kiana@kmcnellis.com>
2016-05-12 00:17:54 -0400

    Bump google maps

--- client/app/app.js
+++ client/app/app.js
@@ -27,8 +27,7 @@ angular.module('venueApp', [
   })
   .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
-        v: '3.20', //defaults to latest 3.X anyhow
+        v: '3.22', //defaults to latest 3.X anyhow
         libraries: 'geometry,visualization,places,marker'
     });
   })
