ba64088b7e6af3ac79575395a2b7f97a0ec9beed
joey <leej42@rpi.edu>
2016-05-12 12:31:50 -0400

    added background image to section creation

--- client/app/courses/edit/courseedit.controller.js
+++ client/app/courses/edit/courseedit.controller.js
@@ -1,8 +1,33 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('CourseEditCtrl', function ($scope, Course, $routeParams) {
+  .controller('CourseEditCtrl', function ($scope, Course, $http, $routeParams) {
     Course.get({id: $routeParams.id}, (course) => {
       $scope.course = course;
+      $scope.coursesLoaded  = true;
     });
+
+    $scope.getImageUrl = function(){
+      if($scope.coursesLoaded){
+        if(! $scope.imgRetrieve && $scope.coursesLoaded && $scope.course.imageURLs[0].startsWith('/api/')){
+          $scope.imgRetrieve = true;
+          console.log("In");
+          $http.get($scope.course.imageURLs[0], {responseType: 'arraybuffer'}, )
+          .then((response) => {
+            var imageBlob = new Blob([response.data], { type: response.headers('Content-Type') });
+            $scope.imageUrl = (window.URL || window.webkitURL).createObjectURL(imageBlob);
+            return $scope.imageUrl;
+          });
+        }
+        else if($scope.imgRetrieve){
+          console.log("good");
+          return $scope.imageUrl;
+        }
+        else{
+          console.log("regular");
+          return $scope.course.imageURLs[0];
+        }
+      }
+    }
+
   });
--- client/app/courses/sections/create/newsection.controller.js
+++ client/app/courses/sections/create/newsection.controller.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('NewSectionCtrl', function ($scope) {
+  .controller('NewSectionCtrl', function ($scope, Course, $http, $routeParams) {
+    Course.get({id: $routeParams.id}, (course) => {
+      $scope.course = course;
+      $scope.coursesLoaded  = true;
 
+    });
+    $scope.getImageUrl = function(){
+      if($scope.coursesLoaded){
+        if(! $scope.imgRetrieve && $scope.coursesLoaded && $scope.course.imageURLs[0].startsWith('/api/')){
+          $scope.imgRetrieve = true;
+          $http.get($scope.course.imageURLs[0], {responseType: 'arraybuffer'}, )
+          .then((response) => {
+            var imageBlob = new Blob([response.data], { type: response.headers('Content-Type') });
+            $scope.imageUrl = (window.URL || window.webkitURL).createObjectURL(imageBlob);
+            return $scope.imageUrl;
+          });
+        }
+        else if($scope.imgRetrieve){
+          return $scope.imageUrl;
+        }
+        else{
+          return $scope.course.imageURLs[0];
+        }
+      }
+    }
   });
