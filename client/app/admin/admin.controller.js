821ecb9a0d358d1ebd2f9dcc253083544bc923fe
seveibar <seveibar@gmail.com>
2016-05-12 05:32:31 -0400

    user csv upload for admins

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,12 +1,31 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('AdminController', function ($scope, User) {
+  .controller('AdminController', function ($scope, User, Upload) {
     $scope.users = User.query();
+
+    $scope.uploadUserCSV = function(file){
+      console.log("Uploading");
+      if (!file){
+        console.log("null file");
+        return;
+      }
+      console.log(arguments, file);
+      Upload.upload({
+          url: '/api/users/csv',
+          data: {
+            files: [file]
+          },
+          objectKey: '.k',
+          arrayKey: '[i]'
+      }).success(((response) => {
+        console.log("success", response);
+      }));
+    };
 
     $scope.delete = function(user) {
       user.$remove();
       $scope.users.splice(this.users.indexOf(user), 1);
     }
-    
+
   });
