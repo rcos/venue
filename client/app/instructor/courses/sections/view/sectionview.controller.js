1a0bc6c448d6c1754f178a7987af6184e2a17b41
Kiana McNellis <kiana@kmcnellis.com>
2016-05-13 15:26:52 -0400

    Clean up section pages

--- client/app/instructor/courses/sections/view/sectionview.controller.js
+++ client/app/instructor/courses/sections/view/sectionview.controller.js
@@ -33,14 +33,28 @@ angular.module('venueApp')
       $scope.user = user;
       $scope.isStudent = (!user.isInstructor) && Auth.isLoggedIn();
       $scope.isInstructor = user.isInstructor;
+      if ($scope.isStudent){
+        $location.path('courses/'+$routeParams.id+'/sections/'+$routeParams.sectionId);
+      }
       loadPageSection();
     });
+
+    var findStudentSubmission = function(){
+      for(var i=0; i < $scope.section.students.length; i++){
+        $scope.section.students[i].submission = '';
+        for(var j=0; j < $scope.submissions.length; j++){
+          if($scope.section.students[i]._id === $scope.submissions[j].submitter._id){
+            $scope.section.students[i].submission = $scope.submissions[j];
+          }
+        }
+      }
+    };
 
     $scope.loadSectionEventSubmissions = function(event){
       $scope.currentEventSelection = event;
       var sectionEvent = event.sectionEvents.find((sectEvent)=>{
-        return sectEvent.section._id == $routeParams.sectionId;
-      })
+        return sectEvent.section._id === $routeParams.sectionId;
+      });
       $scope.selectedEventId = event._id;
       Submission.getAll({'onlySectionEvent': sectionEvent._id, 'withStudents': true}, (submissions)=>{
         $scope.submissions = submissions;
@@ -48,27 +62,23 @@ angular.module('venueApp')
       });
     };
 
-    function findStudentSubmission(){
-      for(var i=0; i < $scope.section.students.length; i++){
-        $scope.section.students[i].submission = '';
-        for(var j=0; j < $scope.submissions.length; j++){
-          if($scope.section.students[i]._id == $scope.submissions[j].submitter._id){
-            $scope.section.students[i].submission = $scope.submissions[j];
-          }
-        }
-      }
-    }
-
     $scope.verifyPendingStudent = (pendingStudent) => {
       var section = $scope.section;
       Section.update({id: section._id}, {pendingStudent: pendingStudent._id}, () => {
           loadPageSection();
         });
-    }
+    };
 
     $scope.ignorePendingStudent = (pendingStudent) => {
       var section = $scope.section;
       Section.update({id: section._id}, {removePendingStudent: pendingStudent._id}, () => {
+          loadPageSection();
+        });
+    };
+
+    $scope.removeStudent = (pendingStudent) => {
+      var section = $scope.section;
+      Section.update({id: section._id}, {removeStudent: pendingStudent._id}, () => {
           loadPageSection();
         });
     };
@@ -93,5 +103,4 @@ angular.module('venueApp')
     $scope.selectStudent = function(student){
       $scope.selectedStudent = student;
     };
-
   });
