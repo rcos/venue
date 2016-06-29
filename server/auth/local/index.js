96bca5025569a9cc8df4488a8a5d3ac968ddbcb3
seveibar <seveibar@gmail.com>
2016-06-28 23:47:30 -0400

    studentOnly and instructorOnly signin flags fixes #285

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -2,11 +2,14 @@
 
 import express from 'express';
 import passport from 'passport';
+import User from "../../api/user/user.model";
 import {signToken} from '../auth.service';
 
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
+  var studentOnly = req.query.studentOnly;
+  var instructorOnly = req.query.instructorOnly;
   passport.authenticate('local', function(err, user, info) {
     var error = err || info;
     if (error) {
@@ -15,9 +18,17 @@ router.post('/', function(req, res, next) {
     if (!user) {
       return res.status(404).json({message: 'Something went wrong, please try again.'});
     }
+
     var profile = user.profile;
-    var token = signToken(user._id, user.role);
-    res.json({ token, profile });
+    User.findById(profile._id).then((user) => {
+      if (studentOnly === true && profile.isInstructor){
+        res.status(401).json({"message": "user is not a student"});
+      }else if (instructorOnly === true && !profile.isInstructor){
+        res.status(401).json({"message": "user is not an instructor"});
+      }
+      var token = signToken(user._id, user.role);
+      res.json({ token, profile });
+    });
   })(req, res, next)
 });
 
