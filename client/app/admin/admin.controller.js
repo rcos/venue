9c111ab5d387838aca1bb11a6b1f799b4d7e9a16
seveibar <seveibar@gmail.com>
2016-06-29 01:25:52 -0400

    display error/success messages

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -19,7 +19,7 @@ angular.module('venueApp')
           objectKey: '.k',
           arrayKey: '[i]'
       }).success(((response) => {
-        console.log("success", response);
+        $scope.csvResults = response.join("<br/>");
       }));
     };
 
