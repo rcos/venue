3c56ee3b59047903f59099286808802fd6c00732
seveibar <seveibar@gmail.com>
2016-07-03 08:21:21 -0400

    fixes #294 fixes #299 sectionevent deletion, sectionevent 404 and 500 pages

--- client/app/events/events.controller.js
+++ client/app/events/events.controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('venueApp')
-  .controller('EventsCtrl', function ($scope, $location, $routeParams, SectionEvent) {
+  .controller('EventsCtrl', function ($scope, $location, $routeParams, SectionEvent, User) {
+
+    User.get((user)=>{
+      $scope.user = user;
+    });
+
     $scope.event = SectionEvent.get({
       id: $routeParams.id,
       withEventInfo: true,
@@ -12,6 +17,9 @@ angular.module('venueApp')
     },
       sectionEvent => {
         $scope.event = sectionEvent;
+      },
+      err => {
+        $scope.err = err;
       });
 
     $scope.goToEventUpload = (id) => {
@@ -19,9 +27,10 @@ angular.module('venueApp')
     };
 
     $scope.deleteEvent = (event)=> {
-      if (confirm("This will also delete all sections submissions")) {
+      if (confirm("This will also delete all sections submissions, are you sure you'd like to delete this event?")) {
         SectionEvent.delete({id:event._id}, (response)=>{
           $scope.events.splice($scope.events.indexOf(event), 1);
+          $location.path("/instructor/events");
         })
       }
     }
