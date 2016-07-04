bb5406c9fa22da7b0487af1ece71473cd8072fe8
seveibar <seveibar@gmail.com>
2016-07-04 14:12:22 -0400

    don't fail if there's no browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -215,7 +215,7 @@ module.exports = function (grunt) {
             // opens browser on initial server start
             nodemon.on('config:update', function () {
               setTimeout(function () {
-                require('open')('http://localhost:8080/debug?port=5858');
+                //require('open')('http://localhost:8080/debug?port=5858');
               }, 500);
             });
           }
