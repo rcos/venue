94381aa647f3a3c32247922172abc9d3bc2da773
joey <leej42@rpi.edu>
2016-07-16 15:37:13 -0400

    events search page

--- client/app/student/events/studentevents.controller.js
+++ client/app/student/events/studentevents.controller.js
@@ -4,6 +4,10 @@ angular.module('venueApp')
   .controller('StudentEventsCtrl', function ($scope, $location, User, SectionEvent) {
     SectionEvent.getAll({onlyUserSections:'me',withEventInfo:true,withSection:true},(events)=>{
       $scope.events = events;
+      $scope.eventsArray = Object.keys(events)
+        .map(eventId => events[eventId])
+        .filter(evnt => evnt._id)
+        .reduce((a,b) => a.concat(b), []);
     });
     $scope.goToEvent = (event) => {
       $location.path("/events/" + event._id);
