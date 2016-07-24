002d1e3306bd5267ea7d8aacc1d431b3a03709b6
seveibar <seveibar@gmail.com>
2016-07-24 16:15:26 -0400

    authentication module

--- server/api/index.integration.js
+++ server/api/index.integration.js
@@ -5,39 +5,32 @@ before((done) => {
 });
 
 var app = require('..');
-var request = require('supertest');
-var superagent = require('superagent');
+// var request = require('supertest');
+// var superagent = require('superagent');
+var auth = require("../auth/local/test.integration");
 
 var instructorAccount = {
   "email": "bob@bob.com",
   "password": "bob"
 };
 
-describe("simple auth request", ()=>{
-    it("should make an authenticated request", (done) => {
-        var agent = superagent.agent();
-        agent
-        .post('http://127.0.0.1:9000/auth/local')
-        .send(instructorAccount)
-        .end((err, res) => {
-            if (err) throw err;
-            var token = res.body.token;
-            var cookie = res.headers['set-cookie'];
-            console.log(token, cookie);
-            request(app)
-                 .post('/api/courses')
-                 .set('Cookie', cookie)
-                 .set('Authorization', 'Bearer ' + token)
-                 .send({
-                     name: "New Course",
-                     description: "Course desc"
-                 })
-                 .attach('files[0]', './client/assets/images/empac.jpg')
-                 .expect(201)
-                 .end((err, res) => {
-                     done();
-                 })
+before((done)=>{
+    auth.init().then(() => done());
+})
 
-        });
+describe("simple auth request", () => {
+    it("should make an authenticated request", (done) => {
+        auth.instructor.request(app)
+            .post('/api/courses')
+            .send({
+                 name: "New Course",
+                 description: "Course desc"
+             })
+             .attach('files[0]', './client/assets/images/empac.jpg')
+             .expect(201)
+             .end((err, res) => {
+                 console.log(res.body);
+                 done();
+             });
     });
 });
---
+++ server/auth/local/test.integration.js
@@ -0,0 +1,75 @@
+var app = require('../..');
+var request = require('supertest');
+var superagent = require('superagent');
+var studentAccount = {
+  "email": "foo@foo.com",
+  "password": "foo"
+};
+var instructorAccount = {
+  "email": "bob@bob.com",
+  "password": "bob"
+};
+var adminAccount = {
+    "email": "admin@admin.com",
+    "password": "admin"
+};
+
+var studentAuth = {}, instructorAuth = {}, adminAuth = {};
+
+module.exports.init = () => {
+    return Promise.all([
+        getAuth(studentAccount).then((auth) => {
+            studentAuth.cookie = auth.cookie;
+            studentAuth.token = auth.token;
+            return Promise.resolve();
+        }),
+        getAuth(instructorAccount).then((auth) => {
+            instructorAuth.cookie = auth.cookie;
+            instructorAuth.token = auth.token;
+            return Promise.resolve();
+        }),
+        getAuth(adminAccount).then((auth) =>  {
+            adminAuth.cookie = auth.cookie;
+            adminAuth.token = auth.token;
+            return Promise.resolve();
+        })
+    ]);
+};
+
+function wrapAuth(auth, req){
+    return req.set('Cookie', auth.cookie)
+              .set('Authorization', 'Bearer ' + auth.token);
+}
+
+function authRequest(auth){
+    return (app) => {
+        var req = request(app);
+        return {
+            get: (...args) => wrapAuth(auth, req.get(...args)),
+            post: (...args) => wrapAuth(auth, req.post(...args)),
+            patch: (...args) => wrapAuth(auth, req.patch(...args)),
+            del: (...args) => wrapAuth(auth, req.del(...args)),
+            put: (...args) => wrapAuth(auth, req.put(...args))
+        };
+    };
+}
+
+module.exports.student = { request: authRequest(studentAuth) };
+module.exports.instructor = { request: authRequest(instructorAuth) };
+module.exports.admin = { request: authRequest(adminAuth) };
+
+function getAuth(account){
+    return new Promise((resolve, reject) => {
+        var agent = superagent.agent();
+        agent
+        .post('http://127.0.0.1:9000/auth/local')
+        .send(account)
+        .end((err, res) => {
+            if (err) throw err;
+            resolve({
+                token: res.body.token,
+                cookie: res.headers['set-cookie']
+            });
+        });
+    })
+}
