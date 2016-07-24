f157c0d9bb3bdea5c97438060aeecfacbac8a390
seveibar <seveibar@gmail.com>
2016-07-24 15:21:54 -0400

    working auth example

--- server/api/index.integration.js
+++ server/api/index.integration.js
@@ -3,3 +3,41 @@ var seed = require('../config/seed').seed;
 before((done) => {
     seed().then(() => done());
 });
+
+var app = require('..');
+var request = require('supertest');
+var superagent = require('superagent');
+
+var instructorAccount = {
+  "email": "bob@bob.com",
+  "password": "bob"
+};
+
+describe("simple auth request", ()=>{
+    it("should make an authenticated request", (done) => {
+        var agent = superagent.agent();
+        agent
+        .post('http://127.0.0.1:9000/auth/local')
+        .send(instructorAccount)
+        .end((err, res) => {
+            if (err) throw err;
+            var token = res.body.token;
+            var cookie = res.headers['set-cookie'];
+            console.log(token, cookie);
+            request(app)
+                 .post('/api/courses')
+                 .set('Cookie', cookie)
+                 .set('Authorization', 'Bearer ' + token)
+                 .send({
+                     name: "New Course",
+                     description: "Course desc"
+                 })
+                 .attach('files[0]', './client/assets/images/empac.jpg')
+                 .expect(201)
+                 .end((err, res) => {
+                     done();
+                 })
+
+        });
+    });
+});
