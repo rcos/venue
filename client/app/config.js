f5b64c42bd47ba2e1de920031aa6c24254278576
seveibar <seveibar@gmail.com>
2016-08-04 20:24:44 -0400

    data directory and travis fix

--- client/app/config.js
+++ client/app/config.js
@@ -1,11 +1,11 @@
 angular.module('config', [])
 
-.constant('ENV', 'development')
+.constant('ENV', 'test')
 
 .constant('CAS_ENABLED', true)
 
-.constant('LOCAL_LOGIN_ENABLED', false)
+.constant('LOCAL_LOGIN_ENABLED', true)
 
-.constant('DEBUG_LOGIN_ENABLED', false)
+.constant('DEBUG_LOGIN_ENABLED', true)
 
 ;
