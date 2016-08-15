97ecec0547832004eb3f39e4c72cc3101280acd6
seveibar <seveibar@gmail.com>
2016-08-15 02:50:28 -0400

    correct integration tests not running because express not listening issue

--- server/api/index.integration.js
+++ server/api/index.integration.js
@@ -1,5 +1,14 @@
 var seed = require('../config/seed').seed;
 var auth = require("../auth/local/test.integration");
+var app = require("../");
+
+console.log("GOT HERE");
+
+before((done) => {
+  app.on('listening', () => {
+    done();
+  });
+});
 
 before((done) => {
     seed().then(() => done());
