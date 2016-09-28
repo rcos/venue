6da50dc8dd43d37e21fe5afba4c15dd6585a5dd7
Kiana McNellis <kiana@kmcnellis.com>
2016-09-28 12:23:14 -0400

    Migrate navbar options to html

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -23,38 +23,6 @@ export default class NavbarComponent {
       }
     });
 
-
-    this.menu = [
-    {
-      'title': 'Dashboard',
-      'link': '/student/dashboard',
-      'forInstructor': false
-    },{
-      'title': 'Dashboard',
-      'link': '/instructor/dashboard',
-      'forInstructor': true
-    },{
-      'title': 'Upload',
-      'link': '/student/upload',
-      'forInstructor': false
-    },{
-      'title': 'Create Event',
-      'link': '/instructor/newevent',
-      'forInstructor': true
-    },{
-      'title': 'Events',
-      'link': '/instructor/events',
-      'forInstructor': true
-    },{
-      'title': 'Events',
-      'link': '/student/events',
-      'forInstructor': false
-    },{
-      'title': 'Search Courses',
-      'link': '/courses',
-      "noauth": true
-    }];
-
     this.isCollapsed = true;
 
     this.help = () => {
@@ -66,14 +34,6 @@ export default class NavbarComponent {
             $window.location.href = "https://github.com/rcos/venue/wiki";
         }
     };
-  }
-
-  isMenuAccessible(navItem){
-    if (!this.isLoggedIn && !navItem.noauth) return false;
-    if (navItem.forInstructor === undefined) return true;
-    if (this.isStudent && (!navItem.forInstructor)) return true;
-    if (this.isInstructor && (navItem.forInstructor)) return true;
-    return false;
   }
 
   isActive(route) {
