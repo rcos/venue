ae1eb309ffdf91b2f3f4a211a57abf39a4cb0a6f
seveibar <seveibar@gmail.com>
2016-10-19 11:07:43 -0400

    longer timeout to seeding task

--- server/api/index.integration.js
+++ server/api/index.integration.js
@@ -2,17 +2,39 @@ var seed = require('../config/seed').seed;
 var auth = require("../auth/local/test.integration");
 var app = require("../");
 
-before((done) => {
+// describe("initialize testing", () => {
+//   this.timeout(15000);
+//
+//   it('app should be running', (done) => {
+//     if (app.isListening) return done();
+//     app.on('listening', () => {
+//       done();
+//     });
+//   });
+//
+//   it('database should be seeded', (done) => {
+//     seed().then(() => done());
+//   });
+//
+//   it('authentication should be initialized', (done)=>{
+//     auth.init().then(() => done());
+//   });
+// });
+
+before(function (done){
+  this.timeout(15000);
   if (app.isListening) return done();
   app.on('listening', () => {
     done();
   });
 });
 
-before((done) => {
-    seed().then(() => done());
+before(function (done){
+  this.timeout(15000);
+  seed().then(() => done());
 });
 
-before((done)=>{
-    auth.init().then(() => done());
+before(function (done){
+  this.timeout(15000);
+  auth.init().then(() => done());
 });
