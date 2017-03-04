6d8ae363793c99251fafbcd7959b64c1598b3d3d
Kiana McNellis <kiana@kmcnellis.com>
2017-03-04 18:00:00 -0500

    Event filtering by date on dashboard

--- client/app/instructor/dashboard/index.js
+++ client/app/instructor/dashboard/index.js
@@ -4,7 +4,7 @@ import routing from './dashboard.routes';
 
 import InstructorDashboardCtrl from './dashboard.controller';
 
-export default angular.module('venueApp.instructordashboard', ['venueApp.auth', 'venueApp.util', ngRoute, 'directives.eventCard', 'directives.courseCard'])
+export default angular.module('venueApp.instructordashboard', ['venueApp.auth', 'venueApp.util', ngRoute, 'directives.eventCard', 'directives.courseCard', 'directives.dateFilter'])
   .controller('InstructorDashboardCtrl', InstructorDashboardCtrl)
   .config(routing)
   .name;
--- client/components/dateFilter/dateFilter.directive.js
+++ client/components/dateFilter/dateFilter.directive.js
@@ -22,4 +22,24 @@ export default angular.module('directives.dateFilter', [])
       return result;
     };
   })
+  .filter("dateBefore", function() {
+    return function(items, from) {
+      if (!items || !from){
+        return items;
+      }
+      var df = from;
+      var result = [];
+      for (var i=0; i<items.length; i++){
+        var current = items[i];
+        for (var j=0; j<current.times.length; j++){
+          var tf = new Date(current.times[j].end);
+          if (tf < df)  {
+            result.push(current);
+            break;
+          }
+        }
+      }
+      return result;
+    };
+  })
   .name;
