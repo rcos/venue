c6683bd3d10e66e9e3a74bdaa3e2d17f4520b1ea
Kiana McNellis <kiana@kmcnellis.com>
2017-03-29 22:47:08 -0400

    Couse cards can redirect to sections

--- client/components/courseCard/courseCard.directive.js
+++ client/components/courseCard/courseCard.directive.js
@@ -12,11 +12,25 @@ export default angular.module('directives.courseCard', [showImage, pictureBanner
         data: '=',
         short: '=',
         sections: '=',
-        click: '&'
+        expandedsection: "="
       },
       link: function (scope, element, attrs) {
       },
-      controller: function ($scope, $element) {
+      controller: function ($scope, $element, Auth) {
+        $scope.isInstructor = Auth.isInstructorSync();
+
+        $scope.viewCourse = function(course){
+          return "/courses/" + course._id;
+        };
+        $scope.viewSection = (course, section) => {
+          if($scope.isInstructor){
+            return "/instructor/courses/"+ course._id + "/sections/" + section._id;
+          }
+          else{
+            return "/courses/"+ course._id + "/sections/" + section._id;
+          }
+        };
+
       }
     };
   })
