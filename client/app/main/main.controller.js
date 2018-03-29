30c4a0f5fd94c73c174bbca94652c72236eabdc7
tayloa <halfnote1004@gmail.com>
2018-03-29 17:33:37 -0400

    Changed splash page. No longer accessible by logged in users.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,16 +2,22 @@
 
 export default class MainController {
   /*@ngInject*/
-  constructor($scope, Auth) {
+  constructor($scope,$window, Auth) {
     $scope.loggedIn = false;
     $scope.isStudent = false;
     $scope.isInstructor = false;
     Auth.getCurrentUser((user) => {
       $scope.user = user;
       $scope.loggedIn = Auth.isLoggedInSync();
-      if ($scope.loggedIn){
+      if ($scope.loggedIn) {
         $scope.isStudent = Auth.isStudentSync();
         $scope.isInstructor = Auth.isInstructorSync();
+        if ($scope.isStudent) {
+          $window.location.href = "/student/dashboard";
+        }
+        if ($scope.isInstructor) {
+          $window.location.href = "/instructor/dashboard";
+        }
       }
     });
   }
