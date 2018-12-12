b4726dbb2674bd33168a53b1d9b068561f9b5b1c
SId <spideysidrao@gmail.com>
2018-12-11 20:09:20 -0500

    small changes

--- client/app/student/grading/grading.controller.js
+++ client/app/student/grading/grading.controller.js
@@ -3,6 +3,8 @@ export default class GradingCtrl {
 
   /*@ngInject*/
   constructor($scope, $location, User, Auth, Submission) {
+    //This retreives the appropriate submissions with this user as the TA
+    //Doesn't work yet
     $scope.refreshSubmissions = function() {
       Submission.getAll({'onlyTA':'me', 'withStudents': true, 'withSection':true, 'withSectionCourse':true}, (submissions)=>{
         $scope.submissions = submissions;
